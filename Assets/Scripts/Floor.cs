6a3c6f5f75338ecdf1f6bc1f7255d6431faffffe
Ronja <git@ronja.space>
2021-03-01 15:45:15 +0100

    initial commit
    a bunch of assets
    player movement
    player death
    camera code

---
+++ Assets/Scripts/Floor.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Floor : MonoBehaviour {
+    [SerializeField] private Transform FollowObject;
+    [SerializeField] private float MaxHeightDiff;
+
+    private Transform trans;
+    
+    private void Awake() {
+        trans = transform;
+    }
+
+    private void LateUpdate()
+    {
+        if (FollowObject.position.y - trans.position.y > MaxHeightDiff)
+            trans.position = trans.position.Y(FollowObject.position.y - MaxHeightDiff);
+    }
+}
---
+++ Assets/Scripts/VectorUtil.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public static class Util {
+	public static Vector3 X(this Vector3 vector, float newX) {
+		vector.x = newX;
+		return vector;
+	}
+	
+	public static Vector3 Y(this Vector3 vector, float newY) {
+		vector.y = newY;
+		return vector;
+	}
+	
+	public static Vector3 Z(this Vector3 vector, float newZ) {
+		vector.z = newZ;
+		return vector;
+	}
+	
+	public static Vector2 X(this Vector2 vector, float newX) {
+		vector.x = newX;
+		return vector;
+	}
+	
+	public static Vector2 Y(this Vector2 vector, float newY) {
+		vector.y = newY;
+		return vector;
+	}
+}
